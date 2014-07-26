9c127641acbd4c8c5919090286232494bb1fe8b7
tvanesse <vanessethomas@gmail.com>
2014-07-27 00:45:18 +0200

    Added the license statements.

--- p3.py
+++ p3.py
@@ -1,3 +1,22 @@
+"""
+Project Euler - Problem 3
+Copyright (C) 2014  Thomas Vanesse
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
 from pylab import *
 
 def sieve_of_Eratosthene(n):
@@ -100,7 +119,6 @@ def p3(N):
 	Acc = {}
 	
 	def loop(N, Acc):
-		print("Current N : " + str(N))
 		s = sqrt(N)
 		
 		for p in primes_memo:
